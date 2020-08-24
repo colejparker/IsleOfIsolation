dc793b23212d7fe7616bd9063fffd41b8f34d8f1
COLEPARKERPC\Cole <cole.parker@hotmail.ca>
2020-08-23 20:59:10 -0600

    Updated EditPlayer to use lists. Getting all the right info but not rendering

--- Assets/Scripts/PlayerEditImage.cs
+++ Assets/Scripts/PlayerEditImage.cs
@@ -9,115 +9,142 @@ public class PlayerEditImage : MonoBehaviour
     [SerializeField] Image Skin;
     [SerializeField] Image Eyes;
     public Image Hair;
-    public int currentHairIndex = 0;
     [SerializeField] Transform HairChoiceParent;
     public Image Shirt;
-    public int currentShirtIndex = 0;
     [SerializeField] Transform ShirtChoiceParent;
     public Image Pants;
-    public int currentPantsIndex = 0;
     [SerializeField] Transform PantsChoiceParent;
     public Image Shoes;
-    public int currentShoesIndex = 0;
     [SerializeField] Transform ShoesChoiceParent;
     [SerializeField] Image Beard;
     [SerializeField] Image Glasses;
+
+    List<Image> imageHairChoices = new List<Image>();
+    List<Image> imageShirtChoices = new List<Image>();
+    List<Image> imageShoesChoices = new List<Image>();
+    List<Image> imagePantsChoices = new List<Image>();
+
+    List<SpriteRenderer> spriteHairChoices = new List<SpriteRenderer>();
+    List<SpriteRenderer> spriteShirtChoices = new List<SpriteRenderer>();
+    List<SpriteRenderer> spriteShoesChoices = new List<SpriteRenderer>();
+    List<SpriteRenderer> spritePantsChoices = new List<SpriteRenderer>();
+
 
     public Player player;
 
     public void CreatePlayerImage(Player incomingPlayer)
     {
         player = incomingPlayer;
+
+        MakeLists();
+
         this.Beard.enabled = player.Beard.enabled;
         this.Beard.color = player.Beard.color;
         this.Glasses.enabled = player.Glasses.enabled;
         this.Skin.color = player.Skin.color;
         this.Eyes.color = player.Eyes.color;
-        Shirt = GetImageFromPlayer(player.ShirtChoiceParent.GetComponentsInChildren<SpriteRenderer>(), ShirtChoiceParent.GetComponentsInChildren<Image>(), currentShirtIndex);
+
+        Shirt = imageShirtChoices[spriteShirtChoices.IndexOf(player.Shirt)];
         Shirt.color = player.Shirt.color;
         Shirt.enabled = true;
-        Pants = GetImageFromPlayer(player.PantsChoiceParent.GetComponentsInChildren<SpriteRenderer>(), PantsChoiceParent.GetComponentsInChildren<Image>(), currentPantsIndex);
+        Pants = imagePantsChoices[spritePantsChoices.IndexOf(player.Pants)];
         Pants.color = player.Pants.color;
         Pants.enabled = true;
-        Shoes = GetImageFromPlayer(player.ShoesChoiceParent.GetComponentsInChildren<SpriteRenderer>(), ShoesChoiceParent.GetComponentsInChildren<Image>(), currentShoesIndex);
+        Shoes = imageShoesChoices[spriteShoesChoices.IndexOf(player.Shoes)];
         Shoes.color = player.Shoes.color;
         Shoes.enabled = true;
-        Hair = GetImageFromPlayer(player.HairChoiceParent.GetComponentsInChildren<SpriteRenderer>(), HairChoiceParent.GetComponentsInChildren<Image>(), currentHairIndex);
+        Hair = imageHairChoices[spriteHairChoices.IndexOf(player.Hair)];
         Hair.color = player.Hair.color;
         Hair.enabled = true;
     }
 
-    private void ChangeElement(int incOrDec, ref Image elementToChange, ref int elementIndex, Transform parent)
+    private void MakeLists()
+    {
+        imageHairChoices = MakeImageListFromArray(HairChoiceParent.GetComponentsInChildren<Image>());
+        imageShirtChoices = MakeImageListFromArray(ShirtChoiceParent.GetComponentsInChildren<Image>());
+        imageShoesChoices = MakeImageListFromArray(ShoesChoiceParent.GetComponentsInChildren<Image>());
+        imagePantsChoices = MakeImageListFromArray(PantsChoiceParent.GetComponentsInChildren<Image>());
+
+        spriteHairChoices = MakeSpriteListFromArray(player.HairChoiceParent.GetComponentsInChildren<SpriteRenderer>());
+        spriteShoesChoices = MakeSpriteListFromArray(player.ShoesChoiceParent.GetComponentsInChildren<SpriteRenderer>());
+        spriteShirtChoices = MakeSpriteListFromArray(player.ShirtChoiceParent.GetComponentsInChildren<SpriteRenderer>());
+        spritePantsChoices = MakeSpriteListFromArray(player.PantsChoiceParent.GetComponentsInChildren<SpriteRenderer>());
+    }
+
+    private List<Image> MakeImageListFromArray(Image[] imageArray)
+    {
+        List<Image> imageList = new List<Image>();
+        foreach(Image image in imageArray)
+        {
+            imageList.Add(image);
+        }
+        return imageList;
+    }
+
+    private List<SpriteRenderer> MakeSpriteListFromArray(SpriteRenderer[] imageArray)
+    {
+        List<SpriteRenderer> imageList = new List<SpriteRenderer>();
+        foreach (SpriteRenderer image in imageArray)
+        {
+            imageList.Add(image);
+        }
+        return imageList;
+    }
+
+    private void ChangeElement(int incOrDec, ref Image elementToChange, List<Image> imageList)
     {
         Color color = elementToChange.color;
-        Image[] choices = parent.GetComponentsInChildren<Image>();
-        if (incOrDec>0)
+        elementToChange.color = color;
+        int currentIndex = imageList.IndexOf(elementToChange);
+        if (incOrDec > 0)
         {
-            elementIndex++;
-            if (elementIndex >= choices.Length)
+            currentIndex++;
+            if (currentIndex>=imageList.Count)
             {
-                elementIndex = 0;
+                currentIndex = 0;
             }
         } else
         {
-            elementIndex--;
-            if (elementIndex < 0)
+            currentIndex--;
+            if (currentIndex < 0)
             {
-                elementIndex = choices.Length - 1;
+                currentIndex = imageList.Count-1;
             }
         }
-        elementToChange = choices[elementIndex];
+        elementToChange = imageList[currentIndex];
         elementToChange.color = color;
-        
     }
 
     public void NextHair()
     {
-        ChangeElement(1, ref Hair, ref currentHairIndex, HairChoiceParent);
+        ChangeElement(1, ref Hair, imageHairChoices);
     }
     public void NextShoes()
     {
-        ChangeElement(1, ref Shoes, ref currentShoesIndex, ShoesChoiceParent);
+        ChangeElement(1, ref Shoes, imageShoesChoices);
     }
     public void NextShirt()
     {
-        ChangeElement(1, ref Shirt, ref currentShirtIndex, ShirtChoiceParent);
+        ChangeElement(1, ref Shirt, imageShirtChoices);
     }
     public void NextPants()
     {
-        ChangeElement(1, ref Pants, ref currentPantsIndex, PantsChoiceParent);
+        ChangeElement(1, ref Pants, imagePantsChoices);
     }
     public void PrevHair()
     {
-        ChangeElement(-1, ref Hair, ref currentHairIndex, HairChoiceParent);
+        ChangeElement(-1, ref Hair, imageHairChoices);
     }
     public void PrevShoes()
     {
-        ChangeElement(-1, ref Shoes, ref currentShoesIndex, ShoesChoiceParent);
+        ChangeElement(-1, ref Shoes, imageShoesChoices);
     }
     public void PrevShirt()
     {
-        ChangeElement(-1, ref Shirt, ref currentShirtIndex, ShirtChoiceParent);
+        ChangeElement(-1, ref Shirt, imageShirtChoices);
     }
     public void PrevPants()
     {
-        ChangeElement(-1, ref Pants, ref currentPantsIndex, PantsChoiceParent);
-    }
-
-
-    private static Image GetImageFromPlayer(SpriteRenderer[] sprites, Image[] images, int currentIndex)
-    {
-        Image imageToReturn = null;
-        int counter = 0;
-        foreach (SpriteRenderer s in sprites)
-        {
-            if (s.enabled)
-            {
-                imageToReturn = images[counter];
-                currentIndex = counter;
-            }
-            counter++;
-        }
-        return imageToReturn;
+        ChangeElement(-1, ref Pants, imagePantsChoices);
     }
 }
